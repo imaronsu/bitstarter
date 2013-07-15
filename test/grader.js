2a3f8a25b20ee4cbc7ef472bc484253e5387089b
Aron Su <imaronsu@gmail.com>
2013-07-15 22:19:33 +0000

    HW3 Part 3 with URL

--- test/grader.js
+++ test/grader.js
@@ -24,16 +24,14 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://floating-caverns-7262.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
@@ -63,48 +61,14 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-var buildfn = function(checksfile) {
-    var response2console = function(result, response) {
-        if (result instanceof Error) {
-            console.error("Error");
-        } else {
-	    $ = cheerio.load(result);
-	    var checks = loadChecks(checksfile).sort();
-	    var out = {};
-
-	    for(var ii in checks) {
-		var present = $(checks[ii]).length > 0;
-		out[checks[ii]] = present;
-	    }
-	    
-	    var outJson = JSON.stringify(out, null, 4);
-	    console.log(outJson);
-	}
-    };
-    return response2console;
-};
-
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-u, --url <url_address>', 'URL address')
         .parse(process.argv);
-
-    if (program.file && program.url) {
-	console.log("Cannot define both --file and --url");
-	process.exit(1);
-    } else if (program.url) {
-	program.url = (true === program.url) ? URL_DEFAULT : program.url;
- 	var response2console = buildfn(program.checks);
-	rest.get(program.url).on('complete', response2console);
-    } else {
-	program.file = (true === program.file) ? HTMLFILE_DEFAULT : program.file;
-	assertFileExists(program.file);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson); 
-    }
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
